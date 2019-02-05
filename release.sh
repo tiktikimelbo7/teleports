71f13e6d4dc61935ddf81128999368e190957e4b
Florian Leeber <flori@bin.org.in>
2019-02-05 11:51:49 +0000

    Update release.sh

--- release.sh
+++ release.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 [ -z "$CI_COMMIT_TAG" ] && exit 0
 [ "$CI_COMMIT_REF_NAME" eq "release-dev" ] && /usr/bin/curl -H "JOB_TOKEN: $CI_JOB_TOKEN" --data '{ "name": "Development Release $CI_COMMIT_TAG", "tag_name": "$CI_COMMIT_TAG", "description": "Recurring TELEports developer release, only for testing!" }' --request POST http://gitlab.com/api/v4/projects/8146239/releases
