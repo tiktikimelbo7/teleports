8f30f2f2c11e0260da4c67f18fc450cac491c53c
Florian Leeber <flori@bin.org.in>
2019-02-07 19:27:04 +0000

    Update release.sh

--- release.sh
+++ release.sh
@@ -3,5 +3,5 @@ echo Tag: $CI_COMMIT_TAG
 echo Branch:$CI_COMMIT_REF_NAME
 apt-get update && apt-get install curl -y
 [ -z "$CI_COMMIT_TAG" ] && exit 0
-[[ $CI_COMMIT_REF_NAME == devrel* ]] && curl -H "JOB_TOKEN: $CI_JOB_TOKEN" --data '{ "name": "Development Release $CI_COMMIT_TAG", "tag_name": "$CI_COMMIT_TAG", "description": "Recurring TELEports developer release, only for testing!" }' --request POST http://gitlab.com/api/v4/projects/8146239/releases
-[[ $CI_COMMIT_REF_NAME == release* ]] && curl -H "JOB_TOKEN: $CI_JOB_TOKEN" --data \'{ "name": "Release $CI_COMMIT_TAG", "tag_name": "$CI_COMMIT_TAG", "description": "This is the current stable release of TELEports" }\' --request POST http://gitlab.com/api/v4/projects/8146239/releases    
+[[ $CI_COMMIT_REF_NAME == devrel* ]] && curl -v -H "JOB_TOKEN: $CI_JOB_TOKEN" --data '{ "name": "Development Release $CI_COMMIT_TAG", "tag_name": "$CI_COMMIT_TAG", "description": "Recurring TELEports developer release, only for testing!" }' --request POST http://gitlab.com/api/v4/projects/8146239/releases
+[[ $CI_COMMIT_REF_NAME == release* ]] && curl -v -H "JOB_TOKEN: $CI_JOB_TOKEN" --data \'{ "name": "Release $CI_COMMIT_TAG", "tag_name": "$CI_COMMIT_TAG", "description": "This is the current stable release of TELEports" }\' --request POST http://gitlab.com/api/v4/projects/8146239/releases    
