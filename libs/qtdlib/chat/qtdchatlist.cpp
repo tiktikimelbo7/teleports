352b76463f63543aa926e70abc7bcfd270f61085
Michele <miguel2000@livecom.it>
2020-06-04 14:19:05 +0200

    added chatList

---
+++ libs/qtdlib/chat/qtdchatlist.cpp
@@ -0,0 +1,18 @@
+#include "chat/qtdchatlist.h"
+
+QTdChatList::QTdChatList(QObject *parent)
+    : QTdObject(parent)
+{
+}
+
+QTdChatListMain::QTdChatListMain(QObject *parent)
+    : QTdChatList(parent)
+{
+    setType(CHAT_LIST_MAIN);
+}
+
+QTdChatListArchive::QTdChatListArchive(QObject *parent)
+    : QTdChatList(parent)
+{
+    setType(CHAT_LIST_ARCHIVE);
+}
