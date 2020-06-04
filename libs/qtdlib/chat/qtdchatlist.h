05a2f222f30b3c6c10ba2b96a920a7b1548c5b95
Michele <miguel2000@livecom.it>
2020-06-04 14:19:05 +0200

    added chatList

---
+++ libs/qtdlib/chat/qtdchatlist.h
@@ -0,0 +1,46 @@
+#ifndef QTDCHATLIST_H
+#define QTDCHATLIST_H
+
+#include <QObject>
+#include "common/qabstracttdobject.h"
+
+/**
+ * @brief The QTdChatList class
+ *
+ * https://core.telegram.org/tdlib/docs/classtd_1_1td__api_1_1_chat_list.html
+ */
+class QTdChatList : public QTdObject
+{
+    Q_OBJECT
+    Q_DISABLE_COPY(QTdChatList)
+public:
+    explicit QTdChatList(QObject *parent = nullptr);
+};
+
+/**
+ * @brief The QTdChatListMain class
+ *
+ * https://core.telegram.org/tdlib/docs/classtd_1_1td__api_1_1chat_list_main.html
+ */
+class QTdChatListMain : public QTdChatList
+{
+    Q_OBJECT
+    Q_DISABLE_COPY(QTdChatListMain)
+public:
+    explicit QTdChatListMain(QObject *parent = Q_NULLPTR);
+};
+
+/**
+ * @brief The QTdChatListArchive class
+ *
+ * https://core.telegram.org/tdlib/docs/classtd_1_1td__api_1_1chat_list_archive.html
+ */
+class QTdChatListArchive : public QTdChatList
+{
+    Q_OBJECT
+    Q_DISABLE_COPY(QTdChatListArchive)
+public:
+    explicit QTdChatListArchive(QObject *parent = Q_NULLPTR);
+};
+
+#endif // QTDCHATLIST_H
