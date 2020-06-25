f4f9345155d4263cac82b6f25d79ed5e3e06366b
Michele <miguel2000@livecom.it>
2020-06-25 16:57:28 +0200

    request chats from chatListArchived

--- libs/qtdlib/chat/requests/qtdgetchatsrequest.cpp
+++ libs/qtdlib/chat/requests/qtdgetchatsrequest.cpp
@@ -5,13 +5,15 @@ QTdGetChatsRequest::QTdGetChatsRequest(QObject *parent)
     : QTdRequest(parent)
     , m_offset_chat_id(0)
     , m_offset_order(std::numeric_limits<std::int64_t>::max())
+    , m_chatList(QStringLiteral("chatListMain"))
 {
 }
 
 QJsonObject QTdGetChatsRequest::marshalJson()
 {
-    return QJsonObject{
+    return QJsonObject {
         { "@type", "getChats" },
+        { "chat_list", QJsonObject { { "@type", m_chatList } } },
         { "offset_order", QString::number(m_offset_order) },
         { "offset_chat_id", m_offset_chat_id },
         { "limit", 50 },
@@ -26,6 +28,11 @@ void QTdGetChatsRequest::setOffsetOrder(const qint64 value)
 void QTdGetChatsRequest::setOffsetChatId(const qint64 value)
 {
     m_offset_chat_id = value;
+}
+
+void QTdGetChatsRequest::setChatList(const QString chatList)
+{
+    m_chatList = chatList;
 }
 
 QFuture<QTdResponse> QTdGetChatsRequest::sendAsync()
